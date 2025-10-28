18293c70747f86ca0e1dd31f27064bf67212e0e9
Bhavya rajput <bhavyarajput6007@gmail.com>
2025-10-28 08:05:13 +0530

    Add files via upload

---
+++ hackman game.py
@@ -0,0 +1,33 @@
+#in this game we chose a correct word
+import random
+word_list = ["apple","mango","banana"]
+lives = 6
+chosen_word = random.choice(word_list)
+print("you have only 6 moves")
+print(chosen_word)
+
+display = []
+for i in range(len(chosen_word)):
+    display +="_"
+print(display)
+
+
+while True:
+    guessed_letter = input("guess a latter").lower()
+    for position in range(len(chosen_word)):
+        letter = chosen_word[position]
+        if letter == guessed_letter:
+            display[position]=guessed_letter
+    print(display)
+
+    if guessed_letter not in chosen_word:
+        lives -=1
+        print(f"now you have only {lives} lives")
+        if lives==0:
+            print("you lose!")
+            break
+    if '_' not in display:
+        print("you win!")
+        break
+
+
